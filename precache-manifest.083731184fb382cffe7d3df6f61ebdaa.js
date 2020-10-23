11e82af71c535c3212b999caf5b5ceae81b87d23
Amol Khedekar <amlu9421@gmail.com>
2020-10-23 12:49:50 +0530

    Updates

---
+++ precache-manifest.083731184fb382cffe7d3df6f61ebdaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc04f16a8f65f9c3f271ea8da312d1c8",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "db2a2702f8542d5cd1be",
+    "url": "/random-quotes/static/css/main.1e59479a.chunk.css"
+  },
+  {
+    "revision": "f2535fcdaa3675a739f2",
+    "url": "/random-quotes/static/js/2.c67821e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/random-quotes/static/js/2.c67821e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2a2702f8542d5cd1be",
+    "url": "/random-quotes/static/js/main.9548c977.chunk.js"
+  },
+  {
+    "revision": "862ed5d0a939d691bb33",
+    "url": "/random-quotes/static/js/runtime-main.a00f302c.js"
+  },
+  {
+    "revision": "4b12f15eacdcae6c8f9dad35d7f7e878",
+    "url": "/random-quotes/static/media/logo.4b12f15e.svg"
+  },
+  {
+    "revision": "e45621a3a3b87b4a22084a849fe5cff6",
+    "url": "/random-quotes/static/media/quote.e45621a3.svg"
+  }
+]);
